87d67cae56a6feb0b9db0283e7ce991b9499a58c
Yussuf Faisal <thecrusheda@gmail.com>
2023-10-08 02:56:56 +0700

    Delete some useless file and not yet for fixing the select2

--- resources/views/pages/strukturorganisasi/index.blade.php
+++ resources/views/pages/strukturorganisasi/index.blade.php
@@ -55,7 +55,6 @@
                                 @endforeach
                             </td>
                             <td>{{ $item->updated_at }}</td>
-                            <td></td>
                             {{-- <td>
                                 @foreach ($list['role'] as $val)
                                     @if ($item->id == $val->id_user)
@@ -63,16 +62,16 @@
                                     @endif
                                 @endforeach
                             </td> --}}
-                            {{-- <td>
+                            <td>
                                 <center>
                                     <div class='btn-group'>
                                         <button type='button'
-                                            class='btn btn-sm btn-primary btn-icon dropdown-toggle hide-arrow'
+                                            class='btn btn-sm btn-outline-dark btn-icon dropdown-toggle hide-arrow'
                                             data-bs-toggle='dropdown' aria-expanded='false'><i
                                                 class='bx bx-dots-vertical-rounded'></i></button>
                                         <ul class='dropdown-menu dropdown-menu-end'>
                                             <li><a href='javascript:void(0);' class='dropdown-item text-warning'
-                                                    onclick="window.location.href='{{ url('hakakses/datakaryawan/' . $item->id . '') }}'"><i
+                                                    onclick="window.location.href='{{ url('strukturorganisasi/' . $item->id . '/ubah') }}'"><i
                                                         class="fa-fw fas fa-edit nav-icon"></i> Ubah</a></li>
                                             <li><a href='javascript:void(0);' class='dropdown-item text-danger'
                                                     onclick="hapus({{ $item->id }})"><i
@@ -80,7 +79,7 @@
                                         </ul>
                                     </div>
                                 </center>
-                            </td> --}}
+                            </td>
                         </tr>
                     @endforeach
                 @endif
@@ -117,7 +116,7 @@
         function hapus(id) {
             Swal.fire({
                 title: 'Apakah anda yakin?',
-                text: 'Hapus Permanen Akun Pengguna ID : ' + id,
+                text: 'Hapus Struktur Organisasi ID : ' + id,
                 icon: 'warning',
                 reverseButtons: false,
                 showDenyButton: false,
@@ -131,7 +130,7 @@
             }).then((result) => {
                 if (result.isConfirmed) {
                     $.ajax({
-                        url: "/api/hakakses/datakaryawan/hapus/" + id,
+                        url: "/api/strukturorganisasi/hapus/" + id,
                         type: 'GET',
                         dataType: 'json', // added data type
                         success: function(res) {
