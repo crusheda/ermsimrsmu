e51b1d19513e2ee6e44da1acd9514f7bbd99d8e2
Yussuf Faisal <thecrusheda@gmail.com>
2023-12-17 23:37:37 +0700

    update modalTambah Aset

--- app/Http/Controllers/Inventaris/Aset/AsetController.php
+++ app/Http/Controllers/Inventaris/Aset/AsetController.php
@@ -12,6 +12,7 @@ use App\Models\aset_peminjaman;
 use App\Models\aset_pengembalian;
 use App\Models\aset_penarikan;
 use App\Models\aset_pemeliharaan;
+use App\Models\roles;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Auth;
@@ -21,6 +22,16 @@ class AsetController extends Controller
 {
     function index()
     {
-        return view('pages.inventaris.aset.index');
+        $role = roles::where('name', '<>','administrator')->orderBy('updated_at','desc')->get();
+        $year = Carbon::now()->isoFormat('YYYY');
+        $ruangan = aset_ruangan::get();
+
+        $data = [
+            'role' => $role,
+            'year' => $year,
+            'ruangan' => $ruangan,
+        ];
+
+        return view('pages.inventaris.aset.index')->with('list',$data);
     }
 }
--- resources/views/pages/inventaris/aset/index.blade.php
+++ resources/views/pages/inventaris/aset/index.blade.php
@@ -114,9 +114,94 @@
                     </div>
                     <div class="modal-body">
                         <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label class="form-label mb-2">Nomor Inventaris</label>
+                                    {{-- <input type="text" class="form-control" placeholder="YYYY-MM-DD" name="tgl_surat" readonly/> --}}
+                                    <h5 class="mb-1">00.03.27.<a id="kd_ruangan" class="text-danger"> . . . </a>.{{ $list['year'] }}</h5>
+                                    <small>Apabila nomor surat tidak sesuai, silakan klik <a href="javascript:void(0);" onclick="reloadBrowser()"><kbd>REFRESH</kbd></a></small>
+                                    <input type="text" class="form-control" id="no_inventaris" hidden>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Ruangan / Lokasi <a class="text-danger">*</a></label>
+                                    <div class="select2-dark">
+                                        <select class="select2 form-select" id="ruangan_add" data-allow-clear="true" data-bs-auto-close="outside" style="width: 100%" required multiple>
+                                            {{-- <option value="all">Seluruh Karyawan</option> --}}
+                                            @if(count($list['ruangan']) > 0)
+                                                @foreach($list['ruangan'] as $item)
+                                                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                                @endforeach
+                                            @endif
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Jenis Aset <a class="text-danger">*</a></label>
+                                    <select class="form-select" id="jenis" required>
+                                        <option value="" hidden>Pilih</option>
+                                        <option value="1">Medis</option>
+                                        <option value="2">Non Medis</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Kalibrasi <a class="text-danger">*</a></label>
+                                    <input type="text" id="kalibrasi" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
+                                </div>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">No. Kalibrasi <a class="text-danger">*</a></label>
+                                    <input type="text" id="no_kalibrasi" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
+                                </div>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Tgl. Berlaku <a class="text-danger">*</a></label>
+                                    <input type="text" class="form-control flatpickr" placeholder="YYYY-MM-DD" id="tgl_berlaku"/>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label class="form-label">Tgl. Perolehan <a class="text-danger">*</a></label>
+                                    <input type="text" class="form-control flatpickr" placeholder="YYYY-MM-DD" id="tgl_perolehan"/>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="col-md-12 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Nama Sarana <a class="text-danger">*</a></label>
+                                    <input type="text" id="sarana" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
+                                </div>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Merk <a class="text-danger">*</a></label>
+                                    <input type="text" id="merk" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
+                                </div>
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">Tipe <a class="text-danger">*</a></label>
+                                    <input type="text" id="tipe" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
+                                </div>
+                            </div>
                             <div class="col-md-4">
                                 <div class="form-group">
-                                    <label class="form-label">Tgl. Surat</label>
+                                    <label class="form-label">No. Seri <a class="text-danger">*</a></label>
+                                    <input type="text" id="no_seri" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="col-md-4 mb-3">
+                                <div class="form-group">
+                                    <label class="form-label">sdasdsa <a class="text-danger">*</a></label>
                                     <input type="text" id="tempat" class="form-control" placeholder="e.g. Hotel Syariah Surakarta">
                                 </div>
                             </div>
@@ -141,7 +226,54 @@
     </div>
     <script>
         $(document).ready(function() {
+            // SELECT2
+            var te = $(".select2");
+            te.length && te.each(function() {
+                var es = $(this);
+                es.wrap('<div class="position-relative"></div>').select2({
+                    placeholder: "Pilih",
+                    dropdownParent: es.parent()
+                })
+            });
 
+            // DATEPICKER
+                // DATE
+                const today = new Date();
+                var tomorrow = new Date(today);
+                tomorrow.setDate(tomorrow.getDate() + 2);
+                var next = new Date(today);
+                next.setDate(next.getDate() + 999999);
+                const l = $('.flatpickr');
+                const ln = $('.flatpickrNull');
+                // const dates = new Date(Date.now());
+                // const tomorow = dates.getTime();
+                // const m = new Date(Date.now());
+                // const c = new Date(Date.now() + 1728e5); // 3 hari kedepan
+                var now = moment().locale('id').format('Y-MM-DD HH:mm');
+                l.flatpickr({
+                    enableTime: 0,
+                    minuteIncrement: 1,
+                    // monthSelectorType: "static",
+                    // inline: true,
+                    // defaultHour: 12,
+                    // defaultMinute: "today",
+                    time_24hr: true,
+                    // dateFormat: "Y-m-d H:m",
+                    disable: [{
+                        from: tomorrow.toISOString().split("T")[0],
+                        to: next.toISOString().split("T")[0]
+                    }]
+                })
+                ln.flatpickr({
+                    enableTime: 0,
+                    defaultDate: now,
+                    minuteIncrement: 1,
+                    time_24hr: true,
+                    disable: [{
+                        from: tomorrow.toISOString().split("T")[0],
+                        to: next.toISOString().split("T")[0]
+                    }]
+                })
         })
 
         // FUNCTION AREA
