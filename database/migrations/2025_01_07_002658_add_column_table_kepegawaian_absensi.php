511c24e5ead4cbd3bd2a07415620f5762b5f902f
Yussuf Faisal <thecrusheda@gmail.com>
2025-01-07 00:33:32 +0700

    Add Column keterlambatan
    table kepegawaian_absensi

---
+++ database/migrations/2025_01_07_002658_add_column_table_kepegawaian_absensi.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnTableKepegawaianAbsensi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('kepegawaian_absensi', function (Blueprint $table) {
+            $table->integer('jenis')->after('id')->comment('1:Shift; 2:Dinas Luar; 3:Tugas Khusus;');
+            $table->time('lembur')->after('ref_jam_pulang')->nullable();
+            $table->time('keterlambatan')->after('ref_jam_pulang')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('kepegawaian_absensi', function (Blueprint $table) {
+            $table->dropColumn('jenis');
+            $table->dropColumn('keterlambatan');
+            $table->dropColumn('lembur');
+        });
+    }
+}
